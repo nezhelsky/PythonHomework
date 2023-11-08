cc2e27e1efcf91669343a38d838581d6fa1923e4
nezhelsky <110371052+nezhelsky@users.noreply.github.com>
2023-11-08 10:03:49 +0600

    Домашка от 6 ноября

---
+++ 23_nov_06/Week2.py
@@ -0,0 +1,11 @@
+print("Mon", end=", ")
+print("Tue", end=", ")
+print("Wed", end=", ")
+print("Thu", end=", ")
+print("Fri", end=", ")
+print("Sat", end=", ")
+print("Sun")
+
+'''
+функция print() выводит название дня недели, параметр end= после названия ставит ", "
+'''
