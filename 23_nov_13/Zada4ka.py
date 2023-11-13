f5555cd0feaf01d2985cebad5406ffdf5a2a3f2d
nezhelsky <110371052+nezhelsky@users.noreply.github.com>
2023-11-13 21:46:51 +0600

    3 урок

---
+++ 23_nov_13/Zada4ka.py
@@ -0,0 +1,9 @@
+user_in1 = input("Input number 1: ")
+user_in2 = input("Input number 2: ")
+user_in3 = input("Input number 3: ")
+
+user_number1, user_number2, user_number3 = int(user_in1), int(user_in2), int(user_in3)
+
+среднее_арифметическое = (user_number1 + user_number2 + user_number3) // 3
+
+print("Среднее арифметическое: ", среднее_арифметическое)
